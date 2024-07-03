1dfc49e85ba251683954045706b738dd51c7a230
Waldeley <163173564+Waldeley@users.noreply.github.com>
2024-07-02 21:53:55 -0300

    Add files via upload

---
+++ Projeto/main.js
@@ -0,0 +1,43 @@
+Grupo GN missão IA>js main.js
+const caixaPrincipal = document. querySelector(".caixa-principal");
+const caixaPrincipal = document. querySelector(".caixa-perguntas");
+const caixaPrincipal = document. querySelector(".caixa-alternativa");
+const caixaPrincipal = document. querySelector(".caixa-resultado");
+const caixaPrincipal = document. querySelector(".texto-resultado");
+const perguntas = [
+{
+enunciado: "A inteligência artificial (IA) é um conjunto de
+tecnologias que permitem aos computadores executar uma variedade de
+funções avançadas. Quais são as implicações da IA na privacidade dos
+dados?",
+alternativas: [
+"A IA, embora possa desempenhar um papel fundamental na detecção e
+prevenção de ameaças à segurança",
+"Também pode ser usada como uma ferramenta para a invasão de
+privacidade",
+]
+},
+{
+enunciado: "Inteligência artificial criando artistas 100% virtuais;
+músicas, obra de arte,incentivando o consumo não consciente prejudicando
+a tradição e cultura. Como preservar a tradição e a cultura no atual
+contexto regido pelas inteligências artificiais",
+alternativas: [
+"Realizando pesquisas para saber sobre suas tradições e culturas.",
+"Também pode ser usada como uma ferramenta para a invasão de
+privacidade.",
+]
+},
+{
+enunciado: "A inteligência artificial (IA) é um conjunto de
+tecnologias que permitem aos computadores executar uma variedade de
+funções avançadas. Quais são as implicações da IA na privacidade dos
+dados?",
+alternativas: [
+"A IA, embora possa desempenhar um papel fundamental na detecção e
+prevenção de ameaças à segurança,",
+"Também pode ser usada como uma ferramenta para a invasão de
+privacidade",
+]
+}
+]
